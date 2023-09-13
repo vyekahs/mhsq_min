0c16cbac7d08065b2ee1d583b5229b7351f80453
민경호 <1736s@naver.com>
2023-09-13 16:21:00 +0900

    feat: breakpoint 잡음

--- src/component/Reservation/병원선택.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-  export let name: string;
-</script>
-
-<div>병원선택 {name}</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,8 +4,11 @@
 
 <style scoped>
   .container {
+    width: 100%;
+    max-width: 576px;
     display: flex;
     flex-direction: column;
     align-items: center;
+    justify-content: center;
   }
 </style>
